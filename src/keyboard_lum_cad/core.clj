fe4dbf706b718401eb1164ec3c1de30534daf098
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2021-09-25 04:51:06 +0900

    :green_heart: add keycap mock test

--- src/keyboard_lum_cad/core.clj
+++ src/keyboard_lum_cad/core.clj
@@ -4,7 +4,8 @@
   (:require [scad-clj.scad :refer :all]
             [scad-clj.model :refer :all]
             [keyboard-lum-cad.mugen :as mugen]
-            [keyboard-lum-cad.mount-hole :as mhole]))
+            [keyboard-lum-cad.mount-hole :as mhole]
+            [keyboard-lum-cad.keycap-mock :as kmock]))
 
 (defn -main
   [& _args]
@@ -12,4 +13,6 @@
   (spit "things/parts/mugen.scad"
         (write-scad mugen/mugen-test))
   (spit "things/parts/mount-hole.scad"
-        (write-scad mhole/mount-test)))
+        (write-scad mhole/mount-test))
+  (spit "things/parts/keycap-mock.scad"
+        (write-scad kmock/keycap-mock)))
