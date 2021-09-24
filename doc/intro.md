c38159e77c155469fc3f20c39d982e9176f23504
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2021-09-24 17:07:34 +0900

    init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to keyboard_lum_cad
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/keyboard_lum_cad/core_test.clj
@@ -0,0 +1,7 @@
+(ns keyboard-lum-cad.core-test
+  (:require [clojure.test :refer :all]
+            [keyboard-lum-cad.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
