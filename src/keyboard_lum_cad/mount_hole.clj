bcbd679a1078b3e545e594461c7c79f6df4ffd15
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2021-09-25 04:36:52 +0900

    :sparkles: add mount hole for cherry MX

---
+++ src/keyboard_lum_cad/mount_hole.clj
@@ -0,0 +1,42 @@
+(ns keyboard-lum-cad.mount-hole
+  (:refer-clojure :exclude [use import])
+  (:require [scad-clj.scad :refer :all]
+            [scad-clj.model :refer :all]))
+
+;; define variables
+(def hole-size-real 14.0)
+(def nail-height 1.4)
+(def nail-width 5.0)
+(def nail-size 0.5)
+
+(def hole-size-err (* hole-size-real 0.05))
+
+(def hole-size (+ hole-size-real hole-size-err))
+
+(def hole-height 3.0)
+(def wall-width 2.25)
+
+(def mount-hole
+  "mount hole object for cherry MX switch"
+  (let [square-out (square (+ hole-size (* wall-width 2))
+                           (+ hole-size (* wall-width 2))
+                           :center true)
+        square-hole (square hole-size hole-size :center true)
+        square-hollow (difference square-out square-hole)
+
+        hollow-box (extrude-linear {:height hole-height :center false}
+                                   square-hollow)
+
+        nail-void-height (max (- hole-height nail-height) 0)
+
+        square-nail-void (->> (square nail-size nail-width :center true)
+                              (translate [(/ 2 (+ hole-size nail-size)) 0 0]))
+        nail-void-oneside (extrude-linear {:height nail-void-height :center false}
+                                          square-nail-void)
+        nail-void (union nail-void-oneside
+                         (mirror [1 0 0] nail-void-oneside))
+
+        mount-hole (->> (difference hollow-box
+                                    nail-void)
+                        (translate [0 0 (- hole-height)]))]
+    mount-hole))
