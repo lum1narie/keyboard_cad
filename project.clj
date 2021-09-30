9ac3060f90b8864002ba4bc829db009720f2366c
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2021-10-01 02:31:10 +0900

    :tada: add matrix component

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [scad-clj "0.5.3"]]
+                 [scad-clj "0.5.3"]
+                 [net.mikera/vectorz-clj "0.48.0"]
+                 [net.mikera/core.matrix "0.62.0"]]
   :main ^:skip-aot keyboard-lum-cad.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
